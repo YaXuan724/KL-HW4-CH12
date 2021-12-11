1db845a3da2220a3776d5692ffcfc9eec8711fad
YaXuan724 <t108360120@ntut.org.tw>
2021-12-12 03:28:22 +0800

    v1.0

--- app/src/main/java/com/example/ch12/MyService.kt
+++ app/src/main/java/com/example/ch12/MyService.kt
@@ -3,24 +3,30 @@ package com.example.ch12
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import kotlinx.coroutines.*
+import kotlinx.coroutines.delay as coroutinesDelay
 
 class MyService : Service() {
+
     override fun onCreate() {
         super.onCreate()
-        Thread{
+
+        val intent = Intent(this,SecActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+        GlobalScope.launch(Dispatchers.Main){
             try {
-                Thread.sleep(3000)
-                val intent = Intent(this,SecActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                coroutinesDelay(3000)
                 startActivity(intent)
-            }catch (e:InterruptedException){
+            } catch (e: InterruptedException){
                 e.printStackTrace()
             }
         }.start()
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    override fun onStartCommand(intent: Intent,flags: Int,startid: Int) : Int {
         return START_NOT_STICKY
     }
+
     override fun onBind(intent: Intent): IBinder? = null
 }
